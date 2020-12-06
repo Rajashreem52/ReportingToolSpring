4a62b4c85fb32de38289a0f58812b5646195e292
ReportingTool_Team_2 <rajashreem52@gmail.com>
2020-12-05 19:20:32 -0600

    static

---
+++ src/main/resources/static/precache-manifest.e75f2b93cd9c07c067ad268b87f06f0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a01c4f507e6f778caf099d6cc927026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f32c75a23f97cf5db7c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "40a51a0e5d6cfc534d11",
+    "url": "/static/js/2.db44361d.chunk.js"
+  },
+  {
+    "revision": "273c4ef3243037f5827d8f065b38b974",
+    "url": "/static/js/2.db44361d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f32c75a23f97cf5db7c",
+    "url": "/static/js/main.5eb7f2fc.chunk.js"
+  },
+  {
+    "revision": "9e3c91a533f8c27ce0c0",
+    "url": "/static/js/runtime-main.929bf24c.js"
+  }
+]);
